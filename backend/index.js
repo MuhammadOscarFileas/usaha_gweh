8b7723fa71c2f921d1c953489f4d233918fc3034
MuhammadOscarFileas <oscarfileasj@gmail.com>
2025-06-25 00:08:23 +0700

    debug cors

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware global
 app.use(cors({
-  origin: ["https://usaha-gweh.vercel.app"],
+  origin: ["https://usaha-gweh.vercel.app", "https://abira-net.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
